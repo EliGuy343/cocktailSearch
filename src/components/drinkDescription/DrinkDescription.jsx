10c9fac144d1a5609ab057a4426d81adf9a21f8a
baronoflulz <baronoflulz@gmail.com>
2022-02-15 21:55:58 +0200

    shut the fuck up, Nietzche

--- src/components/drinkDescription/DrinkDescription.jsx
+++ src/components/drinkDescription/DrinkDescription.jsx
@@ -21,6 +21,21 @@ function DrinkDescription() {
                         <img src={curr_drink.strDrinkThumb}/>
                         <span>{curr_drink.strInstructions}</span>
                     </div>
+                    <div className='second'>
+                        <h2>Ingridents:</h2>
+                        {curr_drink.strIngredient1 && <span>{curr_drink.strIngredient1}</span>}
+                        {curr_drink.strIngredient2 && <span>{curr_drink.strIngredient2}</span>}
+                        {curr_drink.strIngredient3 && <span>{curr_drink.strIngredient3}</span>}
+                        {curr_drink.strIngredient4 && <span>{curr_drink.strIngredient4}</span>}
+                        {curr_drink.strIngredient5 && <span>{curr_drink.strIngredient5}</span>}
+                        {curr_drink.strIngredient6 && <span>{curr_drink.strIngredient6}</span>}
+                        {curr_drink.strIngredient7 && <span>{curr_drink.strIngredient7}</span>}
+                        {curr_drink.strIngredient8 && <span>{curr_drink.strIngredient8}</span>}
+                        {curr_drink.strIngredient9 && <span>{curr_drink.strIngredient9}</span>}
+                        {curr_drink.strIngredient10 && <span>{curr_drink.strIngredient10}</span>}
+                        {curr_drink.strIngredient11 && <span>{curr_drink.strIngredient11}</span>}
+                        {curr_drink.strIngredient12 && <span>{curr_drink.strIngredient12}</span>}
+                    </div>
                 </div>)
 )
   
